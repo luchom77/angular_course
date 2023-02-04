068a773b848ad052352f71785a877b8da958e2d5
Luchom7 <luchomooreventi@yahoo.com>
2023-02-04 18:30:05 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './componentes/personas/personas.component';
+import { PersonaComponent } from './componentes/persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/Persona.ts
@@ -0,0 +1,15 @@
+export class Persona {
+  nombre: string;
+  apellido: string;
+  edad: number;
+
+  constructor(nombre: string = '', apellido: string = '', edad: number = 0) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+
+  cumplirAnios(): void {
+    this.edad++;
+  }
+}
