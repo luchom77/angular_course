40af7cc05e43354442351e8e10a3f9c40ff88d35
Luchom7 <luchomooreventi@yahoo.com>
2023-02-05 16:25:45 -0300

    tercer commit

--- src/app/componentes/personas/personas.component.ts
+++ src/app/componentes/personas/personas.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/Persona';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styleUrls: ['./personas.component.css']
+  styleUrls: ['./personas.component.css'],
 })
-export class PersonasComponent {
+export class PersonasComponent implements OnInit {
+  //atributos
+  personas: Persona[] = [];
 
+  //constructor
+  constructor() {}
+
+  //comportamiento o metodos
+
+  ngOnInit(): void {
+    let persona1 = new Persona('Lucho', 'Moore', 27);
+    this.personas.push(persona1);
+    this.personas.push(new Persona('Alejandro', 'Dolina', 62));
+    this.personas.push(new Persona('Nicky', 'Nicole', 22));
+  }
 }
